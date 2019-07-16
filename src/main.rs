07ec6f04d4a0f8c4550edd72035c78bd9fd9737b
sarashin <sarashina.nikki65@gmail.com>
2019-07-16 12:48:52 +0900

    add skelton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+    .expect("Faild to read line");
+    println!("You guessed: {}", guess);
 }
